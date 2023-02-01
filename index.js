cf4517cd3abb3e836ba6cc9a4db276a3a4bc214f
dolsen0 <derek.olsen@gmail.com>
2023-02-01 15:01:27 -0500

    image url displays on page

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 import express from 'express'
+import { Configuration, OpenAIApi } from 'openai';
 import { apiKey } from "./passkey.js";
 
 const app = express()
 const port = 5857
 
+const configuration = new Configuration({
+    apiKey: apiKey
+})
+
+const openai = new OpenAIApi(configuration);
+const response = await openai.createImage({
+    prompt: "Create a modern logo for a software company named 'Olsen Software'",
+    n:1,
+    size: "512x512"
+})
+const image_url = response.data.data[0].url
+
 app.get("/" , (req,res) => {
-    res.send("Logo Generator")
+    res.send(image_url)
 })
 
 app.listen(port, () => {
